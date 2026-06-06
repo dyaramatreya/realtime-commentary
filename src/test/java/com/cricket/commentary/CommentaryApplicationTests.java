32fcbaae8eccb2cd115c8ee2795d493b725b5560
Atreya Niranjan Dyaram <dyaramatreya@Atreyas-MacBook-Pro.local>
2026-06-06 10:00:07 +0530

    feat(commentary): initial commit

---
+++ src/test/java/com/cricket/commentary/CommentaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricket.commentary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
